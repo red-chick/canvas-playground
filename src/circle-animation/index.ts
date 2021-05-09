9b14d2233afb993b5dadf4b619f2c01b47d1c81a
seungyoub <sy1009yk@naver.com>
2021-05-09 13:44:24 +0900

    오타 수정

--- src/circle-animation/index.ts
+++ src/circle-animation/index.ts
@@ -12,7 +12,7 @@ class App {
 
   constructor() {
     this.canvas = document.createElement("canvas");
-    this.context = this.canvas.getContext("2d")!;
+    this.context = this.canvas.getContext("2d");
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.startTime = Date.now();
